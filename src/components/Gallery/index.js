eb82f970ef58417bccdabf2cd37eabb32d31c561
Remix007 <forvard16back@gmail.com>
2023-02-25 01:08:01 +0200

    Update index.js

--- src/components/Gallery/index.js
+++ src/components/Gallery/index.js
@@ -1,1 +1,117 @@
 export { default } from './Gallery';
+import { useState, useEffect, useRef } from 'react';
+import style from './Gallery.module.css';
+import ImageGalleryItem from 'components/ImageGalleryItem';
+import PropTypes from 'prop-types';
+import Loader from 'components/Loader';
+import Modal from 'components/Modal';
+import Button from 'components/Button';
+import { getImages } from '../Servises/PictureAPI';
+
+function Gallery({
+    imageName,
+   
+  }) {
+    const [images, setImages] = useState([]);
+    const [showModal, setShowModal] = useState(false);
+    const [error, setError] = useState(null);
+    const [page, setPage] = useState(1);
+    const [lageImage, setLageImage] = useState('');
+    const [status, setStatus] = useState('idle');
+  
+    const imageNameRef = useRef(imageName);
+
+    useEffect(() => {
+        const fetchImages = async () => {
+          const isNewSearch = imageName !== imageNameRef.current;
+    
+          if (!imageName || (!isNewSearch && page === 1)) {
+            return;
+          }
+    
+          try {
+            const currentPage = imageName !== imageNameRef.current ? 1 : page;
+            const data = await getImages(imageName, currentPage).then(
+              data => data.hits
+            );
+            if (isNewSearch) {
+              setPage(1);
+              imageNameRef.current = imageName;
+            }
+
+            setImages(prevImages => {
+                const images = isNewSearch ? [] : prevImages;
+                return [...images, ...data];
+              });
+              setStatus('resolved');
+            } catch (error) {
+              setError(error);
+              setStatus('rejected');
+            }
+          };
+      
+          if (imageName) {
+            fetchImages();
+          }
+          
+        }, [imageName, page]);
+      
+        const onClickImage = event => {
+          const findImage = images.find(
+            image => image.id.toString() === event.currentTarget.id
+          );
+          setLageImage(findImage.largeImageURL);
+          toggleModal();
+        };
+        const onClickLoadMore = () => {
+          setPage(prevState => prevState + 1);
+        };
+        const toggleModal = () => {
+          setShowModal(!showModal);
+        };
+
+        if (status === 'idle') {
+            return <h1 className={style.title}>Enter name of images please</h1>;
+          }
+          if (status === 'pending') {
+            return <Loader />;
+          }
+          if (status === 'rejected') {
+            return <h2>{error}</h2>;
+          }
+          if (status === 'resolved') {
+            return (
+              <>
+                <ul className={style.gallery}>
+                  {images?.map(image => (
+                    <ImageGalleryItem
+                      image={image}
+                      key={image.id}
+                      onClick={onClickImage}
+                    />
+                  ))}
+
+</ul>
+        {<Button onClick={onClickLoadMore} />}
+        {showModal && (
+          <Modal onClose={toggleModal}>
+            <img src={lageImage} alt="lageImage" width={1100} />
+            <button
+              type="button"
+              className={style.buttonCloseModal}
+              onClick={toggleModal}
+            >
+              close
+            </button>
+          </Modal>
+        )}
+      </>
+    );
+  }
+}
+
+export default Gallery;
+
+Gallery.propTypes = {
+  imageName: PropTypes.string.isRequired,
+};
